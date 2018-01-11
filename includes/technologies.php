edb8a7ad8edcec2d1db651d50e2eea47311909f1
Scott Montgomery <scott.montgomery@yodle.com>
2018-01-11 14:03:54 -0600

    updates to technologies and contact form for mobile

--- includes/technologies.php
+++ includes/technologies.php
@@ -1,39 +1,30 @@
-       <?php  $arr = array (
+<?php  $arr = array (
           "sass" => "style",
-          "html5" => "markup",
-          "css3" => "style",
-          "AJAX" => "scripting",
-          "JQuery" => "scripting",
           "JavaScript" => "scripting",
           "bootstrap" => "frameworks",
           "wordpress" => "cms",
-          "gulp" => "task-runner",
-          "angular" => "scripting",
+          "shopify" => "cms",
           "less" => "style",
-          "grunt" => "task-runner",
-          "jinja2" => "scripting",
           "php" => "scripting",
-          "npm" => "task-runner",
           "photoshop" => "image-editing",
-          "illustrator" => "image-editing",
-          "skeleton" => "frameworks",
-          "foundation" => "frameworks"
+          "hotjar" => "analytics"
           );
  ?>
- <div id="tech-panel">
-  <div class="container">
-    <div class="row">
-  <div class="col-xs-12 col-md-7 col-dd-7">
-<h2>Technologies we Use</h2>
-<h3>Webby D LLC is happy to work with any technology that fits your needs but these are the ones we utilize most.</h3>
+<div id="tech-panel">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Technologies we Use</h2>
+                <h3>Webby D LLC is happy to work with any technology that fits your needs but these are the ones we utilize most.</h3>
+            </div>
+            <?php foreach ($arr as $key => $value) { ?>
+            <div class="col-sm-1 col-xs-6 element-item tech <?php echo $value ?>" data-category="techs">
+                <img class="icon" src="wp-content/themes/scottrmontgomery/images/techs/<?php echo $key ?>.png" alt="<?php echo $key ?>">
+                <div class="tech-exp">
+                    <?php echo $key ?>
+                </div>
+            </div>
+            <?php }; ?>
+        </div>
+    </div>
 </div>
-  <div class="col-xs-12">
-    <div class="grid">
-      <?php foreach ($arr as $key => $value) { ?>
-      <div class="element-item tech <?php echo $value ?>" data-category="techs">
-        <img class="icon" src="wp-content/themes/scottrmontgomery/images/techs/<?php echo $key ?>.png" alt="<?php echo $key ?>"><div class="tech-exp"><?php echo $key ?></div></div><?php }; ?>
-  </div>
-</div>
-</div>
-  </div>
-  </div>
