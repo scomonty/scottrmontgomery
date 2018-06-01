f6aeeee96b5fa89ee9610fedebe7599ff9f585e5
Scott Montgomery <scott.montgomery@yodle.com>
2018-05-31 19:55:13 -0500

    add schema markup and noopener rel tags

--- includes/offer.php
+++ includes/offer.php
@@ -4,7 +4,7 @@
                 <div class="col-md-7 shopify">
                     <h2>Try Shopify free for two weeks</h2>
                     <p>Looking to build an e-Commerce site but not sure which platform is the right one for you? Try Shopify <span class="no-risk">risk free</span> for two weeks.</p>
-                    <p><a class="butters" href="https://www.shopify.com/?ref=webby-d-llc" target="_blank"><button class="btn sendy dwn-btn" role="button"><span class="btn-content">Claim Offer</span><span class="icon"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i></span></button></a></p>
+                    <p><a class="butters" href="https://www.shopify.com/?ref=webby-d-llc" target="_blank" rel="noopener"><button class="btn sendy dwn-btn" role="button"><span class="btn-content">Claim Offer</span><span class="icon"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i></span></button></a></p>
                 </div>
                 <div class="col-md-5">
                     <img src="wp-content/themes/scottrmontgomery/images/comp.png" alt="shopify computer">
--- includes/social.php
+++ includes/social.php
@@ -1,15 +1,15 @@
  <?php  $arr = array (
-          "https://www.facebook.com/scott.montgomery.75470" => "facebook", 
+          "https://www.facebook.com/scott.montgomery.75470" => "facebook",
           "https://www.linkedin.com/in/srm18135" => "linkedin",
           "https://github.com/scomonty" => "github",
-          "https://twitter.com/Scomonty" => "twitter"); 
+          "https://twitter.com/Scomonty" => "twitter");
  ?>
  <div class="soc">
 <div class="socialIcons">
 <ul>
  <?php foreach ($arr as $key => $value) { ?>
  <li>
-        <a href=<?php echo $key ?> target="_blank"><i class="fa fa-<?php echo $value ?>-square" aria-hidden="true"></i><?php }; ?></a></li>
+        <a href=<?php echo $key ?> target="_blank" rel="noopener"><i class="fa fa-<?php echo $value ?>-square" aria-hidden="true"></i><?php }; ?></a></li>
         </ul>
 
     </div><! -- end socialicons -->
